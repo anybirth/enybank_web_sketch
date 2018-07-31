4b92649fa5ba0ad7a17a8060b5e338c1645d661d
tab0902 <mercury.brave.0902@gmail.com>
2018-07-31 14:08:27 +0900

    Add authentication backends

---
+++ accounts/backends.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth import backends
+from .forms import UserForm
+
+class EmailModelBackend(backends.ModelBackend):
+
+    def authenticate(self, request, username=None, password=None):
+        UserModel = get_user_model()
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'email': username + '@anybirth.co.jp'}
+        try:
+            user = UserModel.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            UserModel.set_password(self, raw_password=password)
