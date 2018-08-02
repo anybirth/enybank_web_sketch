ac58ac79057e7314f2355db92809918a0e2c154e
tab0902 <mercury.brave.0902@gmail.com>
2018-08-02 15:01:34 +0900

    Add page login/facebook

--- enybank_web/urls.py
+++ enybank_web/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('social_django.urls')),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
 ]
