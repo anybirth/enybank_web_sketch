c79d95ab435959a167b2e578861c53bc39c1708f
tab0902 <mercury.brave.0902@gmail.com>
2018-08-02 19:35:36 +0900

    Add page /

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
+from django.views import generic
 from django.shortcuts import render
+from . import models, forms
 
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['color_categories'] = models.ColorCategory.objects.all()
+        context['types'] = models.Type.objects.all()
+        return context
