ba64ef4db6681d3c9242127b6a21b29da353f8c4
tab0902 <mercury.brave.0902@gmail.com>
2018-08-03 17:37:02 +0900

    Update admin and views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
+import uuid
 from datetime import date, datetime, timedelta
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models, forms
 
 # Create your views here.
@@ -15,17 +16,17 @@ class IndexView(generic.TemplateView):
         return context
 
 
-class SearchView(generic.TemplateView):
+class SearchView(generic.ListView):
+    model = models.Item
+    context_object_name = 'items'
     template_name = 'main/search.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['item'] = models.Item.objects.get(
+    def fee_calculator(self):
+        item = models.Item.objects.get(
             color_category=self.request.GET.get('color_category'),
             type=self.request.GET.get('type')
         )
 
-        item = context['item']
         intercept = item.fee_intercept
         coefs = item.item_fee_coef_set.order_by('starting_point')
         fee = intercept
@@ -42,14 +43,46 @@ class SearchView(generic.TemplateView):
             if end_point:
                 if days <= end_point:
                     fee += fee_coef * (days - starting_point)
-                    context['fee'] = round(fee, -1)
-                    return context
+                    return round(fee, -1)
                 elif end_point < days:
                     fee += fee_coef * (end_point - starting_point)
             else:
                 fee += fee_coef * (days - starting_point)
-                context['fee'] = round(fee, -1)
-                return context
+                return round(fee, -1)
 
-        context['fee'] = round(fee, -1)
+        return round(fee, -1)
+
+    def get_queryset(self):
+        return models.Item.objects.filter(
+            color_category=self.request.GET.get('color_category'),
+            type=self.request.GET.get('type')
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['fee'] = self.fee_calculator()
         return context
+
+    def post(self, request):
+        if 'cart' in request.session:
+            cart, created = models.Cart.objects.get_or_create(uuid=request.session.get('cart'))
+            if created:
+                request.session['cart'] = cart.uuid
+        else:
+            _uuid = str(uuid.uuid4())
+            request.session['cart'] = _uuid
+            new_cart = models.Cart(uuid=_uuid)
+            new_cart.save()
+            cart = models.Cart.objects.get(uuid=_uuid)
+
+        reservation = models.Reservation(
+            cart=cart,
+            item=models.Item.objects.get(uuid=request.POST.get('item')),
+            start_date=request.GET.get('start_date'),
+            return_date=request.GET.get('return_date'),
+            total_fee=self.fee_calculator(),
+        )
+        if request.user.is_authenticated:
+            reservation.user = request.user
+        reservation.save()
+        return redirect('main:cart', permanent=True)
