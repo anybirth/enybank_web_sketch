c6d21f73f70bdf30317c030d1b34535d30739142
tab0902 <mercury.brave.0902@gmail.com>
2018-08-03 12:13:22 +0900

    Add page search/

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime, timedelta
 from django.views import generic
 from django.shortcuts import render
 from . import models, forms
@@ -11,4 +12,44 @@ class IndexView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['color_categories'] = models.ColorCategory.objects.all()
         context['types'] = models.Type.objects.all()
+        return context
+
+
+class SearchView(generic.TemplateView):
+    template_name = 'main/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['item'] = models.Item.objects.get(
+            color_category=self.request.GET.get('color_category'),
+            type=self.request.GET.get('type')
+        )
+
+        item = context['item']
+        intercept = item.fee_intercept
+        coefs = item.item_fee_coef_set.order_by('starting_point')
+        fee = intercept
+        start_date = datetime.strptime(self.request.GET.get('start_date'), '%Y-%m-%d')
+        return_date = datetime.strptime(self.request.GET.get('return_date'), '%Y-%m-%d')
+        delta = return_date - start_date
+        days = delta.days + 1
+
+        for coef in coefs:
+            fee_coef = coef.fee_coef
+            starting_point = coef.starting_point
+            end_point = coef.end_point
+
+            if end_point:
+                if days <= end_point:
+                    fee += fee_coef * (days - starting_point)
+                    context['fee'] = round(fee, -1)
+                    return context
+                elif end_point < days:
+                    fee += fee_coef * (end_point - starting_point)
+            else:
+                fee += fee_coef * (days - starting_point)
+                context['fee'] = round(fee, -1)
+                return context
+
+        context['fee'] = round(fee, -1)
         return context
