b2c6d4cf062077e3578f758cd21fc544937f2ee8
tab0902 <mercury.brave.0902@gmail.com>
2018-08-04 18:43:05 +0900

    Add reselver_context_processors

--- enybank_web/settings.py
+++ enybank_web/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'enybank_web.context_processors.resolver_context_processor',
             ],
         },
     },
